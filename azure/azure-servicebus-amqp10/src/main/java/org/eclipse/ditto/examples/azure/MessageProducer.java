fe57cf0fa8e8ce554c1becea249bb4362907396e
Kai Zimmermann <kai.zimmermann@microsoft.com>
2019-06-03 09:15:06 +0200

    Wait for message response from DittoSigned-off-by: Kai Zimmermann <kai.zimmermann@microsoft.com>

--- azure/azure-servicebus-amqp10/src/main/java/org/eclipse/ditto/examples/azure/MessageProducer.java
+++ azure/azure-servicebus-amqp10/src/main/java/org/eclipse/ditto/examples/azure/MessageProducer.java
@@ -1,12 +1,11 @@
 /*
  * Copyright (c) 2019 Contributors to the Eclipse Foundation
  *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
+ * See the NOTICE file(s) distributed with this work for additional information regarding copyright
+ * ownership.
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
+ * This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0
  *
  * SPDX-License-Identifier: EPL-2.0
  */
@@ -14,11 +13,16 @@ package org.eclipse.ditto.examples.azure;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import org.awaitility.Awaitility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.jms.annotation.JmsListener;
@@ -35,40 +39,55 @@ public class MessageProducer implements CommandLineRunner {
   @Autowired
   private JmsTemplate jmsTemplate;
 
+  private final Set<String> sendMessages = Collections.synchronizedSet(new HashSet<>());
+
   @Override
   public void run(final String... args) throws Exception {
-    sendMessageFromFile("deleteThingSample.json");
-    sendMessageFromFile("createThingSample.json");
+      for (int i = 0; i < 10; i++) {
+        final String thingAndCorrId = UUID.randomUUID().toString();
+        sendMessageFromFile("createThingSample.json", thingAndCorrId);
+        sendMessageFromFile("deleteThingSample.json", thingAndCorrId);
+      }
+
+      Awaitility.await().atMost(1, TimeUnit.MINUTES).pollDelay(50, TimeUnit.MILLISECONDS)
+        .pollInterval(50, TimeUnit.MILLISECONDS).until(sendMessages::isEmpty);
+
+      System.out.println("Responses from Ditto complete!");
   }
 
-  private void sendMessageFromFile(String fileName) throws IOException {
+  private void sendMessageFromFile(final String fileName, final String correlationId)
+      throws IOException {
     final URL file = Resources.getResource(fileName);
     final String content =
-        CharMatcher.whitespace().removeFrom(Resources.toString(file, Charsets.UTF_8));
-
+        CharMatcher.whitespace().removeFrom(Resources.toString(file, Charsets.UTF_8)).replace("fancy-car-test", correlationId);
 
     jmsTemplate.send("dittoinbound", (MessageCreator) session -> {
       final TextMessage message = session.createTextMessage(content);
 
-      setResponse(session, message);
+      setResponse(session, message, correlationId);
+      sendMessages.add(correlationId);
 
       return message;
     });
 
+
     System.out.println("Sent message to Ditto: " + content);
   }
 
-  private void setResponse(Session session, final TextMessage message) throws JMSException {
+  private void setResponse(final Session session, final TextMessage message,
+      final String correlationId) throws JMSException {
     final Destination responseDestination = jmsTemplate.getDestinationResolver()
         .resolveDestinationName(session, "dittoresponses", true);
 
-    message.setJMSCorrelationID(UUID.randomUUID().toString());
+    message.setJMSCorrelationID(correlationId);
     message.setJMSReplyTo(responseDestination);
   }
 
   @JmsListener(destination = "dittoresponses", containerFactory = "myFactory")
-  public void processDittoResponse(final String message) {
+  public void processDittoResponse(final TextMessage message) throws JMSException {
 
-    System.out.println("Response from Ditto: " + message);
+    sendMessages.remove(message.getJMSCorrelationID());
+
+    System.out.println("Response from Ditto " + message.getBody(String.class));
   }
 }
