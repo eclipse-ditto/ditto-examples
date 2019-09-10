3a013ab588f5d2cc4d17b684aaca6c501b900601
Stefan Maute <stefan.maute@bosch-si.com>
2019-09-10 09:19:37 +0200

    update license header;
    Signed-off-by: Stefan Maute <stefan.maute@bosch-si.com>

--- java-client/README.md
+++ java-client/README.md
@@ -19,7 +19,7 @@ The examples are preconfigured to work with a local Eclipse Ditto running in Doc
  [GitHub](https://github.com/eclipse/ditto/tree/master/deployment/docker).
 
 You can change the configuration to your liking by editing `src/main/resources/config.properties`.
-The configured usernames and passwords must be added to the nginx.htpasswd of Eclipse Ditto.
+The configured user names and passwords must be added to the nginx.htpasswd of Eclipse Ditto.
 ```bash
 htpasswd nginx.htpasswd user1
 ```
--- java-client/src/main/java/org/eclipse/ditto/examples/common/model/ExampleUser.java
+++ java-client/src/main/java/org/eclipse/ditto/examples/common/model/ExampleUser.java
@@ -1,28 +1,14 @@
 /*
- *                                            Bosch SI Example Code License
- *                                              Version 1.0, January 2016
+ * Copyright (c) 2019 Contributors to the Eclipse Foundation
  *
- * Copyright 2017 Bosch Software Innovations GmbH ("Bosch SI"). All rights reserved.
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
  *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
- * following conditions are met:
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
  *
- * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
- * disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
- * following disclaimer in the documentation and/or other materials provided with the distribution.
- *
- * BOSCH SI PROVIDES THE PROGRAM "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO
- * THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
- * ALL NECESSARY SERVICING, REPAIR OR CORRECTION. THIS SHALL NOT APPLY TO MATERIAL DEFECTS AND DEFECTS OF TITLE WHICH
- * BOSCH SI HAS FRAUDULENTLY CONCEALED. APART FROM THE CASES STIPULATED ABOVE, BOSCH SI SHALL BE LIABLE WITHOUT
- * LIMITATION FOR INTENT OR GROSS NEGLIGENCE, FOR INJURIES TO LIFE, BODY OR HEALTH AND ACCORDING TO THE PROVISIONS OF
- * THE GERMAN PRODUCT LIABILITY ACT (PRODUKTHAFTUNGSGESETZ). THE SCOPE OF A GUARANTEE GRANTED BY BOSCH SI SHALL REMAIN
- * UNAFFECTED BY LIMITATIONS OF LIABILITY. IN ALL OTHER CASES, LIABILITY OF BOSCH SI IS EXCLUDED. THESE LIMITATIONS OF
- * LIABILITY ALSO APPLY IN REGARD TO THE FAULT OF VICARIOUS AGENTS OF BOSCH SI AND THE PERSONAL LIABILITY OF BOSCH SI'S
- * EMPLOYEES, REPRESENTATIVES AND ORGANS.
+ * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.ditto.examples.common.model;
 
