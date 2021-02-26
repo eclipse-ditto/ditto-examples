293a7574b33ba3ffdb0d784a754a6ed3f0f56631
Gerald Glocker <gerald.glocker@bosch.io>
2021-02-26 12:30:10 +0100

    Connection manager example
    Signed-off-by: Gerald Glocker <gerald.glocker@bosch.io>

---
+++ connection-process-manager/src/connection_refresher.ts
@@ -0,0 +1,230 @@
+/*
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+import * as log from "https://deno.land/std@0.88.0/log/mod.ts";
+// @deno-types="https://unpkg.com/@types/mustache@4.1.0/index.d.ts"
+import Mustache from "https://unpkg.com/mustache@4.1.0/mustache.mjs";
+// @deno-types="https://cdn.jsdelivr.net/npm/@types/jsonpath-plus@5.0.0/index.d.ts"
+import { JSONPath } from "https://cdn.jsdelivr.net/npm/jsonpath-plus@5.0.3/dist/index-browser-esm.min.js";
+import { ProcessManager } from "./process_manager.ts";
+
+interface API {
+  url: string;
+  method: "GET" | "POST";
+  body?: string;
+  unwrapJsonPath?: string;
+}
+
+/**
+ * Lookup infos about managed connections and execute&monitor commands for each of them.
+ **/
+export class ConnectionRefresher {
+  private readonly logger = log.getLogger(ConnectionRefresher.name);
+
+  constructor(
+    public options: {
+      listConnections: API;
+      retrieveConnection: API;
+      retrieveStatus: API;
+      basicAuth?: {
+        user: string;
+        password: string;
+      };
+      oAuth?: {
+        client: string;
+        secret: string;
+        scope: string;
+        tokenUrl: string;
+      };
+      filter: string;
+      connectionMonitorInterval: number;
+      cmdPattern: string[];
+    },
+  ) {
+    if (options.connectionMonitorInterval < 1) {
+      throw new Error(
+        "Invalid value for 'connectionMonitorInterval'; must be at least 1",
+      );
+    }
+  }
+
+  /**
+   * Retrieves connection list and spawns processes for all connections.
+   * Monitoring is restarted automatically in the background in the configured interval.
+   **/
+  async runAndMonitor(pm: ProcessManager) {
+    try {
+      // prepare authentication header
+      let auth;
+      if (this.options.basicAuth) {
+        auth = {
+          "Authorization": "Basic " +
+            btoa(
+              this.options.basicAuth.user + ":" +
+                this.options.basicAuth.password,
+            ),
+        };
+      } else {
+        const accessToken = await this.authenticateoauthClientCredentialsFlow();
+        auth = { "Authorization": "Bearer " + accessToken };
+      }
+
+      // retrieve list of all managed connections and filter it
+      const conns: Info[] = [];
+      for (const id of await this.listConnections(auth)) {
+        conns.push(await this.retrieveInfo(auth, id));
+      }
+      this.logger.debug(() =>
+        `Filtering connections: ${this.options.filter} on ${
+          JSON.stringify(conns)
+        }`
+      );
+      const filtered = JSONPath({
+        path: this.options.filter,
+        json: conns,
+      }) as Info[];
+      const connIds = filtered.map((c) => c.id);
+      this.logger.debug(() => `Connections: ${JSON.stringify(connIds)}`);
+
+      const removedIds = Array.from(pm.keys()).filter((x) =>
+        !connIds.includes(x)
+      );
+
+      // update processes for all current connections
+      for (const id of connIds) {
+        const info = await this.retrieveInfo(auth, id);
+
+        // construct effective command line based on command pattern by replacing placeholders
+        const cmd = this.options.cmdPattern.map((c) =>
+          Mustache.render(c, info)
+        );
+
+        if (!cmd.every((c) => !c || c === "")) {
+          // not fully empty cmd: create/update process
+          pm.set(id, cmd);
+        } else {
+          // empty cmd: delete/skip it
+          pm.delete(id);
+        }
+      }
+
+      // remove processes for all old, removed connections
+      for (const id of removedIds) {
+        pm.delete(id);
+      }
+    } finally {
+      // wait a short moment and restart monitoring
+      setTimeout(
+        () => this.runAndMonitor(pm),
+        this.options.connectionMonitorInterval * 1000,
+      );
+    }
+  }
+
+  /** Retrieve list of all relevant connectionss. */
+  private async listConnections(auth: Record<string, string>) {
+    const r = await this.fetchExt(this.options.listConnections, {}, auth);
+    return (r as Array<{ id: string }>).map((o) => o.id);
+  }
+
+  /** Retrieve connection info of a specific connection. */
+  private async retrieveInfo(
+    auth: { Authorization: string },
+    id: string,
+  ): Promise<Info> {
+    const param = { id: id, idEncoded: encodeURIComponent(id) };
+
+    // lookup connection info
+    const r = await this.fetchExt(this.options.retrieveConnection, param, auth);
+
+    // lookup connection status also and add it as top-level "status" property
+    const s = await this.fetchExt(this.options.retrieveStatus, param, auth);
+    r.connectionStatusDetails = s;
+
+    // enrich further infos
+    this.enrichInfo(r);
+    return r;
+  }
+
+  /** Execute HTTP API by inlining parameters into URL or body and optionally unwrapping result via JsonPath. */
+  private async fetchExt(
+    api: API,
+    params: Record<string, unknown>,
+    headers: Record<string, string>,
+  ): Promise<Info> {
+    const r = await fetch(
+      Mustache.render(api.url, params),
+      {
+        headers: headers,
+        body: api.body ? Mustache.render(api.body, params) : undefined,
+      },
+    );
+    if (!r.ok) {
+      throw new Error(`API call to ${api.url} failed; ${await r.text()}`);
+    }
+    let s = await r.json();
+    if (api.unwrapJsonPath) {
+      s = JSONPath({ path: api.unwrapJsonPath, json: s }) as Array<unknown>[0];
+    }
+    return s;
+  }
+
+  /** Execution authentication for OAuth2 Client Credential Flow. */
+  private async authenticateoauthClientCredentialsFlow() {
+    const r = await fetch(this.options.oAuth!.tokenUrl, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+      method: "POST",
+      body: new URLSearchParams({
+        grant_type: "client_credentials",
+        client_id: this.options.oAuth!.client,
+        client_secret: this.options.oAuth!.secret,
+        scope: this.options.oAuth!.scope,
+      }).toString(),
+    });
+    if (!r.ok) {
+      throw new Error(`Authentication failed; ${await r.text()}`);
+    }
+    const resultJson = await r.json();
+    return resultJson.access_token;
+  }
+
+  /** Enrich connection info with additional convenience information. */
+  private enrichInfo(o: Info) {
+    // expand "uri" content as explicit content of a object "uriDetails"
+    if (o.uri) {
+      const u = new URL(o.uri);
+      let port = u.port;
+      if (!u.port || u.port === "") {
+        if (u.protocol === "https:") port = "443";
+        else if (u.protocol === "http:") port = "80";
+      }
+      o.uriDetails = {
+        port: port,
+        host: u.host,
+        hostname: u.hostname,
+        protocol: u.protocol,
+        username: u.username,
+        password: u.password,
+        pathname: u.pathname,
+      };
+    }
+  }
+}
+
+// deno-lint-ignore no-explicit-any
+export type Info = any;
+
+// disable Mustache escaping
+Mustache.escape = (s) => s;
---
+++ connection-process-manager/src/process_manager.ts
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+import * as log from "https://deno.land/std@0.88.0/log/mod.ts";
+import { equal } from "https://deno.land/std@0.88.0/testing/asserts.ts";
+
+/**
+ * Management of execution of sub-processes.
+ * Makes sure that configured sub-processes are always running and are re-started when they are stopped or when the command line should be replaced.
+ **/
+export class ProcessManager {
+  private readonly logger = log.getLogger(ProcessManager.name);
+  private readonly processes: { [id: string]: ManagedProcess } = {};
+
+  constructor(
+    public options: {
+      processMonitorInterval: number;
+    },
+  ) {
+    if (options.processMonitorInterval < 1) {
+      throw new Error(
+        "Invalid value for 'processMonitorInterval'; must be at least 1",
+      );
+    }
+  }
+
+  /** Execute and monitor all managed processes. Monitoring is restarted automatically in the background in the configured interval. */
+  async runAllAndMonitor() {
+    try {
+      for (const id in this.processes) {
+        const p = this.processes[id];
+
+        if (p.proc) {
+          // process is already running: await result
+          const s = await p.proc.status();
+          const stdout = new TextDecoder().decode(await p.proc.output());
+          const stderr = new TextDecoder().decode(await p.proc.stderrOutput());
+          this.logger.info(
+            `Command for ${id} terminated. PID ${p.proc.pid}: status code ${s.code}`,
+          );
+          if (stdout) this.logger.debug(() => `Command output: ${stdout}`);
+          if (stderr) this.logger.debug(() => `Command error: ${stderr}`);
+
+          p.proc = undefined;
+        }
+
+        // (re-)start process
+        try {
+          p.proc = Deno.run({
+            cmd: p.cmd,
+            stdout: "piped",
+            stderr: "piped",
+            stdin: "null",
+          });
+        } catch (e) {
+          throw new Error(`${e}; command: ${p.cmd.join(" ")}`);
+        }
+        this.logger.info(
+          `Command for ${id} started.    PID ${p.proc.pid}: ${p.cmd.join(" ")}`,
+        );
+      }
+
+      // Flush all log handlers from time to time - otherwise logs may be missed too long
+      // deno-lint-ignore no-explicit-any
+      log.getLogger().handlers.map((h: any) => h.flush && h.flush());
+    } finally {
+      // wait a short moment and restart monitoring
+      setTimeout(
+        () => this.runAllAndMonitor(),
+        this.options.processMonitorInterval * 1000,
+      );
+    }
+  }
+
+  /**
+   * Set or update a process to be managed.
+   * If process is currently running and the settings are changed, then it will be terminated and restarted.
+   **/
+  set(id: string, cmd: string[]): this {
+    if (id in this.processes) {
+      // already managed process
+      const p = this.processes[id];
+
+      if (!equal(p.cmd, cmd)) {
+        // configuration changed
+        this.kill(id, p);
+        p.cmd = cmd;
+      }
+    } else {
+      // new managed process
+      this.processes[id] = new ManagedProcess(cmd);
+    }
+    return this;
+  }
+
+  /** Kill and remove process. */
+  delete(id: string): boolean {
+    if (id in this.processes) {
+      this.kill(id, this.processes[id]);
+      delete this.processes[id];
+      return true;
+    }
+    return false;
+  }
+
+  /** Lists all ids of managed processes. */
+  keys() {
+    return Object.keys(this.processes).values();
+  }
+
+  private kill(id: string, p: ManagedProcess) {
+    if (p.proc) {
+      // process running, so request to terminate (SIGTERM)
+      p.proc.kill(15);
+      this.logger.info(`Send SIGTERM for ${id}. PID ${p.proc.pid}`);
+    }
+  }
+}
+
+class ManagedProcess {
+  proc?: Deno.Process;
+
+  constructor(public cmd: string[]) {}
+}
